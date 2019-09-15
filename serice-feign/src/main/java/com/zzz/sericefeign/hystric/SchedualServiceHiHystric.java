4d4bb972f5b5dcd69d2a72c9a3597146bf3a685e
luzhenwei <876898885@qq.com>
2019-09-15 19:50:31 +0800

    zuul

---
+++ serice-feign/src/main/java/com/zzz/sericefeign/hystric/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package com.zzz.sericefeign.hystric;
+
+import com.zzz.sericefeign.fegin.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
